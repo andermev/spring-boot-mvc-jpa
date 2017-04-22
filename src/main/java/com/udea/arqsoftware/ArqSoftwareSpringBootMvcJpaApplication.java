afbbd8197f6089f6552efdbf8a1a457ecf6a4b42
andermev <ander1298@gmail.com>
2017-04-22 00:21:36 -0500

    Add files via upload
    Commit inicial

---
+++ src/main/java/com/udea/arqsoftware/ArqSoftwareSpringBootMvcJpaApplication.java
@@ -0,0 +1,12 @@
+package com.udea.arqsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqSoftwareSpringBootMvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqSoftwareSpringBootMvcJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/udea/arqsoftware/ArqSoftwareSpringBootMvcJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udea.arqsoftware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArqSoftwareSpringBootMvcJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
